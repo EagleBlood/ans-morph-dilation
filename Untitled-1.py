638740984333e6f889d97f790917553b0a82d87b
Dawid Nosal <35221@student.pwsztar.edu.pl>
2023-03-14 11:51:43 +0100

    main code

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,17 @@
-print("Hej fajny miecz");
+import cv2
+import numpy as np
+
+# wczytanie obrazu
+img = cv2.imread('D:\Programowanie\Python\image.png', 0)
+
+# określenie kernela
+kernel = np.ones((5, 5), np.uint8)
+
+# zastosowanie dylatacji z wykorzystaniem kernela
+dilation = cv2.dilate(img, kernel, iterations=8)
+
+# wyświetlenie obrazów przed i po dylatacji
+cv2.imshow('Original Image', img)
+cv2.imshow('Dilation', dilation)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
