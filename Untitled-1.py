90af92c077496941954bdecd5355be6bf9de7bf3
Dawid Nosal <35221@student.pwsztar.edu.pl>
2023-03-14 12:12:43 +0100

    gui addon

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,6 +3,10 @@ import numpy as np
 
 # wczytanie obrazu
 img = cv2.imread('D:\Programowanie\Python\image.png', 0)
+img = cv2.resize(img, (400, 400))
+
+# konwersja trybu kolorów na 3 kanały
+img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
 # określenie kernela
 kernel = np.ones((5, 5), np.uint8)
@@ -10,8 +14,25 @@ kernel = np.ones((5, 5), np.uint8)
 # zastosowanie dylatacji z wykorzystaniem kernela
 dilation = cv2.dilate(img, kernel, iterations=8)
 
+# dodanie marginesów między obrazami
+img = cv2.copyMakeBorder(img, 0, 0, 0, 0, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+dilation = cv2.copyMakeBorder(dilation, 0, 0, 0, 0, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+
+# utworzenie obrazu szarego jako tła okna
+background = np.zeros((500, 880, 3), dtype=np.uint8)
+background[:] = (128, 128, 128)
+
+# dodanie opisu na górze obrazów
+cv2.putText(background, "Image 1", (80, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(background, "Image 2", (500, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+# umieszczenie obrazów na tle
+background[50:450, 50:450] = img
+background[50:450, 470:870] = dilation
+
 # wyświetlenie obrazów przed i po dylatacji
-cv2.imshow('Original Image', img)
-cv2.imshow('Dilation', dilation)
+cv2.imshow('Result', background)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
