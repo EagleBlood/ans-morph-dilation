58b96e68111f265247bf9de1e69aa25015632d1f
Dawid Nosal <35221@student.pwsztar.edu.pl>
2023-03-20 22:14:31 +0100

    code refractor

--- Untitled-1.py
+++ Untitled-1.py
@@ -10,14 +10,13 @@ def thick(img):
     img3 = np.array(imgA,np.uint8)
     img4 = np.array(imgA,np.uint8)
 
-    tab = np.zeros((3,3), np.uint8)
-    tab[0,1] = 1
-  
-
     masked1 = np.zeros((3,3), np.uint8)
     masked2 = np.zeros((3,3), np.uint8)
     masked3 = np.zeros((3,3), np.uint8)
     masked4 = np.zeros((3,3), np.uint8)
+
+    tab = np.zeros((3,3), np.uint8)
+    tab[0,1] = 1
     
     for y in range(0,img.shape[0]-2):
         for x in range(0,img.shape[1]-2):
@@ -80,33 +79,35 @@ def thick(img):
 
    
 
+#global variables
+margin_size = 70
+white_color = (255, 255, 255)
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 
 # load the input image
 img = cv2.imread(r'ertka.bmp', 0)
 
+
 # apply the morphological dilation
 dilation, step1, step2, step3, step4 = thick(img)
 
-# create a 2x3 table for displaying the images with margins and a gray background
-margin_size = 70
-white_color = (255, 255, 255)
+
+# create a 2x3 table for displaying the images with margins and a background
 table = np.ones((2*img.shape[0] + 3*margin_size, 3*img.shape[1] + 4*margin_size), dtype=np.uint8) * white_color[0]
 table[margin_size:-margin_size, margin_size:-margin_size] = white_color[0]
 
-# set the first image to be the input image with margins and a label
-font = cv2.FONT_HERSHEY_SIMPLEX
+
+# set the images and labels
 cv2.putText(table, 'Input Image', (margin_size+10, margin_size-10), font, 1, (0, 0, 0), 2)
 table[margin_size:img.shape[0]+margin_size, margin_size:img.shape[1]+margin_size] = img
 
-# set the intermediate images in the remaining cells of the first row with margins and labels
 cv2.putText(table, 'Step 1', (img.shape[1]+2*margin_size+10, margin_size-10), font, 1, (0, 0, 0), 2)
 table[margin_size:img.shape[0]+margin_size, img.shape[1]+2*margin_size:2*img.shape[1]+2*margin_size] = step1
 
 cv2.putText(table, 'Step 2', (2*img.shape[1]+3*margin_size+10, margin_size-10), font, 1, (0, 0, 0), 2)
 table[margin_size:img.shape[0]+margin_size, 2*img.shape[1]+3*margin_size:3*img.shape[1]+3*margin_size] = step2
 
-# set the intermediate images in the remaining cells of the second row with margins and labels
 cv2.putText(table, 'Step 3', (margin_size+10, img.shape[0]+2*margin_size-10), font, 1, (0, 0, 0), 2)
 table[img.shape[0]+2*margin_size:2*img.shape[0]+2*margin_size, margin_size:img.shape[1]+margin_size] = step3
 
